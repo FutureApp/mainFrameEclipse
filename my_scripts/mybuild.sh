c6875411a2440a9523bcac2db482b664cec74e45
Your Name <mcz>
2018-02-09 07:31:25 -0500

    assembly init

--- my_scripts/mybuild.sh
+++ my_scripts/mybuild.sh
@@ -4,8 +4,10 @@ cd /home/linux1/work/che && git pull
 sudo docker build -t "my-buntu" /home/linux1/work/che/dockerfiles/my_godBuntu/.
 sudo docker build -t "my-alpine" /home/linux1/work/che/dockerfiles/my_alpine/.
 sudo docker build -t "puppet/puppet-agent-alpine" /home/linux1/work/che/dockerfiles/my_dockers/puppet-agent/.
-	
 sudo docker build -t "maven:3.3-jdk-8" /home/linux1/work/che/dockerfiles/my_godBuntu/.
-cd /home/linux1/work/che/dockerfiles/
-sudo bash ./build.sh
+
+cd /home/linux1/work/ && sudo chmod -v -R 777 che
+
+cd /home/linux1/work/che/assembly/assembly-main  && sudo mvn clean install	
+cd /home/linux1/work/che/dockerfiles/ && sudo  bash build.sh
 #
